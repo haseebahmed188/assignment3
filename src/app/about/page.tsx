55f14212874819d0576dcb139e42d02662a2c3b2
HASEEB AHMED <155947094+haseebahmed188@users.noreply.github.com>
2024-10-02 17:53:29 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,7 +19,7 @@ export default function about(){
         <p>Phone: 03000000000</p>
 
 
-        <p>; 2024 Haseeb All rights reserved.</p>
+        <p>2024 Haseeb All rights reserved.</p>
     </div> 
     </div>
     )
