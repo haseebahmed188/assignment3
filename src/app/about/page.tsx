80a6093ec998f408c328574c5eb9436d9112a5d7
HASEEB AHMED <155947094+haseebahmed188@users.noreply.github.com>
2024-10-02 17:47:16 +0500

    Add files via upload

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import Header from "../../../components/Header"
+
+export default function about(){
+
+    return(
+        <div>
+        <div>
+            <Header />
+        <h1 >Haseeb Ahmed</h1>
+        <h1 >About me "I am a student of GIAIC"</h1>
+        <img className="image" src="https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-1/458650423_1906437439831399_7446194754543969558_n.jpg?stp=dst-jpg_s200x200&_nc_cat=104&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeH6uhKtq4pZktvNEiJsJjLlZpUeOW3WDbFmlR45bdYNsQSa8_OYx25KqEuuzNanAiNuFKV8QhJU2CPYfdCYcaVv&_nc_ohc=dqnREV6F0HAQ7kNvgE94Ips&_nc_ht=scontent.fkhi22-1.fna&_nc_gid=AjDhHC6jIUzSY-dUx6rhGLe&oh=00_AYDEp-A9vLs7YwodjWxQHv-7WDw7bGEcGRlJfXrRy67yNw&oe=670201A1" alt="img" />
+        </div>
+
+
+<div className="footer">
+        <h2>Haseeb Ahmed</h2>
+        <p>Web Developer | Designer</p>
+        <p>Email: exapmle@gmail.com</p>
+        <p>Phone: 03000000000</p>
+
+
+        <p>; 2024 Haseeb All rights reserved.</p>
+    </div> 
+    </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,28 @@
-import Image from "next/image";
+
+import Header from "../../components/Header";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+    <div>
+      <Header />
+    <h1>My name is Haseeb Ahmed</h1>
+    <img className="image" src="https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-1/458650423_1906437439831399_7446194754543969558_n.jpg?stp=dst-jpg_s200x200&_nc_cat=104&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeH6uhKtq4pZktvNEiJsJjLlZpUeOW3WDbFmlR45bdYNsQSa8_OYx25KqEuuzNanAiNuFKV8QhJU2CPYfdCYcaVv&_nc_ohc=dqnREV6F0HAQ7kNvgE94Ips&_nc_ht=scontent.fkhi22-1.fna&_nc_gid=AjDhHC6jIUzSY-dUx6rhGLe&oh=00_AYDEp-A9vLs7YwodjWxQHv-7WDw7bGEcGRlJfXrRy67yNw&oe=670201A1" alt="img" />
     </div>
-  );
+    <div>
+      <div className="footer">
+        <h2>Haseeb Ahmed</h2>
+        <p>Web Developer | Designer</p>
+        <p>Email: exapmle@gmail.com</p>
+        <p>Phone: 03000000000</p>
+
+
+        <p>; 2024 Haseeb All rights reserved.</p>
+    </div>  
+    </div>
+    </div>
+);
+
+
 }
