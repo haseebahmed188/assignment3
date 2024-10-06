12c0d5a13d47e33d2062078608af3474053a4bd7
HASEEB AHMED <155947094+haseebahmed188@users.noreply.github.com>
2024-10-06 19:32:31 +0500

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,13 @@
 
-import Header from "../../components/Header";
-
-
 export default function Home() {
   return (
     <div>
     <div>
-      <Header />
+    
     <h1>My name is Haseeb Ahmed</h1>
-    <img className="image" src="https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-1/458650423_1906437439831399_7446194754543969558_n.jpg?stp=dst-jpg_s200x200&_nc_cat=104&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeH6uhKtq4pZktvNEiJsJjLlZpUeOW3WDbFmlR45bdYNsQSa8_OYx25KqEuuzNanAiNuFKV8QhJU2CPYfdCYcaVv&_nc_ohc=dqnREV6F0HAQ7kNvgE94Ips&_nc_ht=scontent.fkhi22-1.fna&_nc_gid=AjDhHC6jIUzSY-dUx6rhGLe&oh=00_AYDEp-A9vLs7YwodjWxQHv-7WDw7bGEcGRlJfXrRy67yNw&oe=670201A1" alt="img" />
+    <img className="image" src="./" alt="" />
     </div>
-    <div>
-      <div className="footer">
-        <h2>Haseeb Ahmed</h2>
-        <p>Web Developer | Designer</p>
-        <p>Email: exapmle@gmail.com</p>
-        <p>Phone: 03000000000</p>
-
-
-        <p>2024 Haseeb All rights reserved.</p>
-    </div>  
-    </div>
+    
     </div>
 );
 
